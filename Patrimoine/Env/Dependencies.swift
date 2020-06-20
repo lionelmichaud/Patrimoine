2e2765fc6ef89e5cee0d171acbae31ba288141d9
Lionel <lio.michaud@gmail.com>
2020-06-20 16:31:34 +0200

    mise à jour des dépendances

--- Patrimoine/Env/Dependencies.swift
+++ Patrimoine/Env/Dependencies.swift
@@ -1,5 +1,9 @@
 
-/* https://github.com/IgorMuzyka/Type-Preserving-Coding-Adapter.git
-
+/*
+ TypePreservingCodingAdapter: https://github.com/IgorMuzyka/Type-Preserving-Coding-Adapter.git
+ ActivityIndicatorView: https://github.com/exyte/ActivityIndicatorView.git
+ KeyboardAvoider: https://github.com/michaelhenry/KeyboardAvoider.git
+ Charts: https://github.com/danielgindi/Charts.git
+ Disk:   https://github.com/saoudrizwan/Disk.git
 */
 
