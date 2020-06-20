271e05661250179b7fb34f6007e1b4b06710f018
Lionel <lio.michaud@gmail.com>
2020-06-20 16:05:59 +0200

    Initial Commit

---
+++ Patrimoine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Patrimoine
+//
+//  Created by Lionel MICHAUD on 20/06/2020.
+//  Copyright © 2020 Lionel MICHAUD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
