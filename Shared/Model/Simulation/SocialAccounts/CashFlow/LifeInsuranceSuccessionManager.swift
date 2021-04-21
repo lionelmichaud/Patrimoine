277ae236c6aaa9c220ac1bb0d2b84ad009eae100
Lionel <lio.michaud@gmail.com>
2021-04-21 19:55:59 +0200

    Refactoring de Patrimoine
    Extraction des fonctions de gestion des successions de l'extension pour les transformer en Struct de manager

---
+++ Shared/Model/Simulation/SocialAccounts/CashFlow/LifeInsuranceSuccessionManager.swift
@@ -0,0 +1,9 @@
+//
+//  LifeInsuranceSuccessionManager.swift
+//  Patrimoine
+//
+//  Created by Lionel MICHAUD on 21/04/2021.
+//  Copyright © 2021 Lionel MICHAUD. All rights reserved.
+//
+
+import Foundation
---
+++ Shared/Model/Simulation/SocialAccounts/CashFlow/OwnershipManager.swift
@@ -0,0 +1,8 @@
+//
+//  OwnershipManager.swift
+//  Patrimonio
+//
+//  Created by Lionel MICHAUD on 21/04/2021.
+//
+
+import Foundation
